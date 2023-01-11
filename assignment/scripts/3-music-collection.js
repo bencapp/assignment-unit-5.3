180257865d594a9ef422e3f4da0dfbd54c7bb9f0
Ben Capp <bentucker333@gmail.com>
2023-01-11 16:36:07 -0600

    added albums and logged collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,3 +11,16 @@ function addToCollection(title, artist, yearPublished) {
   collection.push(newRecord);
   return newRecord;
 }
+
+console.log(
+  addToCollection("Sgt. Pepper's Lonely Hearts Club Band", "The Beatles", 1967)
+);
+console.log(
+  addToCollection("The Times They Are a-Changin'", "Bob Dylan", 1964)
+);
+console.log(addToCollection("Crushing", "Julia Jacklin", 2019));
+console.log(addToCollection("Deja Vu", "Crosby, Stills, Nash & Young", 1970));
+console.log(addToCollection("Townes Van Zandt", "Townes Van Zandt", 1969));
+console.log(addToCollection("Bruised Orange", "John Prine", 1978));
+
+console.log(collection);

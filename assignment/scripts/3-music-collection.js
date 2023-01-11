1dea5d34ebd30c8f697bde0969f25253f8a96286
Ben Capp <bentucker333@gmail.com>
2023-01-11 16:28:00 -0600

    created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let newRecord = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(newRecord);
+  return newRecord;
+}
